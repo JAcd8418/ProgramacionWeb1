f736ca95582a593ebcfe9c4bf32b6d9ab8cba317
JAcd8418 <101907988+JAcd8418@users.noreply.github.com>
2022-03-24 19:31:17 -0500

    Add files via upload

---
+++ Web1/index.js
@@ -0,0 +1,10 @@
+function getNombre(nombre){
+console.log(nombre)
+}
+getNombre ("Jose Cortes");
+
+function suma(n1, n2){
+    const suma = n1 + n2;
+    console.log(suma);
+}
+suma(10,20);
---
+++ Web1/inicio.js
@@ -0,0 +1,51 @@
+function getMensaje(){
+    
+    return console.log("Hola Grupo");
+    let mensaje = getMensaje();
+}
+getMensaje();
+
+function getMansajeN(){
+    console.log("Hola Mundo");
+}
+getMansajeN();
+
+const getNombre2 = ( ) => {
+    console.log ("Hola Mundo");
+}
+getNombre2();
+
+function sumar (){
+    suma = 10+2;
+    return suma;
+}
+function restar(){
+    resta = 10-2;
+    return resta;
+}
+function multiplicar(){
+    multiplicar = 10*2;
+    return multiplicar;
+}
+function dividir(){
+   const numeroUno = 10;
+   const numeroDos = 0;
+   let division = 0;
+   if (numeroDos === 0){
+       division = "NO se puede dividir por cero";
+    } else{
+        return division = numeroUno/numeroDos;
+    }
+   }
+const resultadoSuma = sumar();
+console.log(resultadoSuma);
+const resultadoResta = restar();
+console.log(resultadoResta);
+const resultadoMultiplicar = multiplicar();
+console.log(resultadoMultiplicar);
+const resultadoDividir = dividir();
+console.log(resultadoDividir);
+
+
+
+
